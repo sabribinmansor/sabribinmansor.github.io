efa935c25d47dc8a5ce4d139eed9f37082797157
saaab_ri <66418607+sabribinmansor@users.noreply.github.com>
2021-08-06 12:22:20 +0800

    Add files via upload

---
+++ tab_datascience.php
@@ -0,0 +1,45 @@
+                <h2>Data, Data, Data</h2>
+                <p class="subtitle-tabcontent">Exploring the realm of data analytics and data engineering.</p>
+                <table class="datascience-table" width="100%">
+
+		    <tr><td colspan="2" class="text-center color-white bg-theme-sand">Data Fundamentals</td></tr>
+                    <tr>
+                    <td colspan="2" class="text-left">
+                        <p>(TODO: Update exercises and python files - describe knowledge gained)</p>
+                    </td></tr>
+                    <tr><td>
+                                <div class="image python_merging_dataframes_pandas"></div>
+				<div class="image python_manipulating_dataframes_pandas"></div>
+                    </td><td class="text-left">
+				<h4>Manipulating & Merging Dataframes</h4>
+                                <p>Learnt a few features of Python</p>
+                    </td></tr>
+                    </td><td>
+                                <div class="image python_data_cleaning"></div>
+                                <div class="image python_pandas_foundation"></div>
+                    </td><td class="text-left">
+                                <h4>Cleaning Data & Pandas Foundations</h4>
+                                <p>Learnt a few features of Python</p>
+                    </td></tr>
+                    <tr><td>
+                            	<div class="image python_datascience_toolbox1"></div>
+                                <div class="image python_datascience_toolbox2"></div>
+                    </td><td class="text-left">
+                             	<h4>Python Data Science</h4>
+                                <p>Learnt a few features of Python</p>
+                    </td></tr>
+                    <tr><td colspan="2" class="text-left">
+                                <h4>Python Fundamentals</h4>
+                                <p>Learnt a few features of Python</p>
+				<table>
+				  <tr>
+				      <td><div class="image python_intermediate"></div></td>
+                                      <td><div class="image python_introduction"></div></td>
+				  </tr>
+				</table>
+				<p>Exercise: Flower(iris) Dataset</p>
+				<p>Sources: <a href="#">link</a></p>
+				<p>Dataset: <a href="#">iris.csv</a></p>
+				<p>Python File: <a href="#">species.py</a></p>
+                    </td></tr>
+                </table>
